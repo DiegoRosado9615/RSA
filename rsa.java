9b745ef825a5b3d1dbdb73723cb2d0cbcf064c74
jonatan <jonatanaacm@ciencias.unam.mx>
2020-05-21 21:39:58 -0500

    Métodos pedidos

---
+++ rsa.java
@@ -0,0 +1,26 @@
+import java.math.BigInteger;
+public class rsa{
+
+	public BigInteger generaLlaves(){
+
+		return null;
+
+	}
+	
+	public static String cifrar(BigInteger n, int e, String mensaje){
+
+
+	}
+
+	public static String descrifrar(BigInteger n, int d, int mensaje){
+
+	}
+
+	public static void main(String[] args) {
+	
+
+
+
+	}
+
+}
