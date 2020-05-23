ec97516c73e2d170b1e7bfe210b3008a37534f23
jonatan <jonatanaacm@ciencias.unam.mx>
2020-05-22 21:58:44 -0500

    avanzando

--- rsa.java
+++ rsa.java
@@ -7,20 +7,44 @@ public class rsa{
 
 	}
 	
-	public static String cifrar(BigInteger n, int e, String mensaje){
-
+	public static byte[] cifrar(BigInteger n, BigInteger e, byte[] mensaje){
+	
+	return (new BigInteger(mensaje).modPow(e,n)).toByteArray();
 
 	}
 
-	public static String descrifrar(BigInteger n, int d, int mensaje){
+	public static String imprimeBytes(byte[] mensaje, String cad){
+		int c = 0;
+		for (byte b : mensaje ) {
+			c++;
+			cad+=Byte.toString(b) + "\t";
+		}
+		return cad+"\t c: "+c;
+	}
 
+	public static byte[]  descrifrar(BigInteger n, BigInteger d, byte [] mensaje){
+
+		return (new BigInteger(mensaje)).modPow(d, n).toByteArray();
+		
 	}
 
 	public static void main(String[] args) {
 	
+		String mensaje="mensaje,";
+		BigInteger n = new BigInteger("2257");
+		BigInteger e = new BigInteger("7");
+		BigInteger d = new BigInteger("1543");
 
 
+		System.out.println("Mensaje en Bytes: "+ imprimeBytes(mensaje.getBytes() , ""));
+		System.out.println("cifrado:");
+		System.out.println();
+		byte [] encripyt = cifrar(n,e,mensaje.getBytes());
+		System.out.println(imprimeBytes(encripyt,""));
+		System.out.println("\n\nDescifrado:\n");
+		System.out.println(imprimeBytes());
 
+		
 	}
 
 }
