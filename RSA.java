34c12f13dafb000b1f6f1fa48c4a148229897ed7
LoboStark <rocadie9615@ciencias.unam.mx>
2020-05-23 16:58:45 -0500

    Ya podemos sacar la llave publica Yeii

--- RSA.java
+++ RSA.java
@@ -4,7 +4,6 @@ import java.util.Random;
 
 class RSA{
    Random r;
-
    /**
    Metodo que crea son los primos de al menos tamaño 50
    @param
@@ -18,14 +17,16 @@ class RSA{
    }
 
    /**
-   *Metodo que permite la creación de la llave privada para el metodo RSa
+   *Metodo que permite la creación de el modulo donde va a vivir nuestro RSA
+   *@param
+   *@return BigInteger
    */
 
    public BigInteger creadorLlave(){
      BigInteger p = creadorPrimos();
      BigInteger q = creadorPrimos();
      BigInteger resta= new BigInteger("-1");
-      while(p==q){ // verifico que p no sea igual a q
+      while((p.compareTo(q))==0){ // verifico que p no sea igual a q
         q=creadorPrimos();
       }
       BigInteger llave = new BigInteger("0");
@@ -33,15 +34,65 @@ class RSA{
       q=q.add(resta);
       llave=llave.add(p);
       llave=llave.add(q);
+      System.out.println("##########################");
+      System.out.println("Esta es la llave " + llave );
      return llave;
    }
+   /**
+   *Metodo que nos permitira saber si un número es primo o no
+   *@param int x
+   *@return
+   */
 
+   public boolean esPrimo(int x){
+    int contador=2;
+    boolean primo =true;
+    while ((contador!=x)){
+      if (x % contador == 0){
+          primo = false;
+          return primo;
+      }
+      contador++;
+    }
+
+    return  primo;
+   }
+
+   /**
+   *Metodo que nos genera un número que sea coprimo de nuesta llave
+   *@param BigInteger x
+   *@return int x
+   */
+   public int cooPrimo(BigInteger x){
+     BigInteger proCop= new BigInteger("0");
+     String cast ="";
+     String sobrante;
+     int esperanza=0;
+     BigInteger modulo;
+     for (int i=0;i<10000 ;i++ ) {
+        if (esPrimo(i)) {
+          cast=i+"";
+          proCop= new BigInteger(cast);
+          modulo=x.mod(proCop);
+          sobrante=modulo.toString();
+          if ("0"!=sobrante) {
+            System.out.println("Este es k " + i );
+            return i;
+          }
+        }
+     }
+     System.out.println("Por favor intente de nuevo :C ");
+     return -1;
+   }
 
 
   public static void main(String[] args) {
     RSA prueba= new RSA();
-    System.out.println(prueba.creadorLlave());
-    //prueba.creadorLlave();
+    BigInteger numeros= prueba.creadorLlave();
+    int x= prueba.cooPrimo(numeros);
+
+
+
   }
 
 }
