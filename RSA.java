9a27560868809a553fb546fca62fefa5c109bc9d
Luis Angel Leyva Castillo <43196753+chechenque@users.noreply.github.com>
2020-05-26 22:46:00 -0500

    ADD Potencia

--- RSA.java
+++ RSA.java
@@ -23,12 +23,14 @@ class RSA{
    /**
    *Metodo que permite la creación de el modulo donde va a vivir nuestro RSA
    *@param
-   *@return BigInteger
+   *@return BigInteger [0] = n
+   *@return BigInteger [1] = z
    */
 
-   public BigInteger creadorLlave(){
+   public BigInteger[] creadorLlave(){
      BigInteger p = creadorPrimos();
      BigInteger q = creadorPrimos();
+     BigInteger [] llaves= new BigInteger[2];
       while((p.compareTo(q))==0){ // verifico que p no sea igual a q
         q=creadorPrimos();
       }
@@ -36,11 +38,13 @@ class RSA{
       BigInteger producto= p.multiply(q);
       System.out.println("##########################");
       System.out.println("Esta es la llave " + producto );
+      llaves[0]=producto;
       p=p.add(MENOSUNO);
       q=q.add(MENOSUNO);
       llave=llave.add(p);
       llave=llave.multiply(q);
-     return llave;
+      llaves[1]=llave;
+     return llaves;
    }
    /**
    *Metodo que nos permitira saber si un número es primo o no
@@ -111,26 +115,12 @@ class RSA{
      BigInteger t2 = UNO;
      BigInteger r = m.mod(a);
      BigInteger x = a;
-     BigInteger y = m;
-     BigInteger c = UNO;
+     BigInteger y = r;
+     BigInteger c ;
      BigInteger t1p;
      BigInteger t2p;
      while ((r.compareTo(CERO)) !=0 ) {
-       System.out.println("c1 "+c1);
-       System.out.println("c2 "+c2);
-       System.out.println("t1 "+t1);
-       System.out.println("t2 "+t2);
-       System.out.println("r "+r);
-       System.out.println("c "+c);
-
-       System.out.println("Bucle");
-       System.out.println("Este es x " + x + " Este es y " + y );
-       if(x.divide(y).compareTo(CERO) == 0){
-        c=UNO;
-       }else{
-        c  = x.divide(y);
-       }
-       System.out.println("hola " + x.divide(y));
+       c  = x.divide(y);
        r  = x.mod(y);
        c1 = (MENOSUNO.multiply(c)).multiply(c1);
        c2 = (MENOSUNO.multiply(c)).multiply(c2);
@@ -138,32 +128,269 @@ class RSA{
        c2 = c2.add(t2);
        t1p = (c1.add(MENOSUNO.multiply(t1)));
        t2p = (c2.add(MENOSUNO.multiply(t2)));
-
-      t1 = (MENOSUNO.multiply(t1p)).divide(c);
-      t2 = (MENOSUNO.multiply(t2p)).divide(c);
-
-      x=y;
-      y=r;
+       t1 = (MENOSUNO.multiply(t1p)).divide(c);
+       t2 = (MENOSUNO.multiply(t2p)).divide(c);
+       x=y;
+       y=r;
      }
-     if(x.compareTo(UNO) ==  0) return t2;
+     if(x.compareTo(UNO) ==  0) return t2.mod(m);
      else{
        System.out.println("No tuvo inverso lo siento F");
        return MENOSUNO;
      }
-
    }//inversoMultiplicativo
+
+   /**
+   Metodo auxiliarque recibe un char y lo transforma en número
+   *@param char x
+   *@return String y
+   */
+   public String letraNum(char x){
+     switch(x){
+       case 'a':
+       return "0";
+
+       case 'b' :
+       return "1";
+
+       case 'c':
+       return "2";
+
+       case 'd' :
+       return "3";
+
+       case 'e':
+       return "4";
+
+       case 'f' :
+       return "5";
+
+       case 'g':
+       return "6";
+
+       case 'h' :
+       return "7";
+
+       case 'i':
+       return "8";
+
+       case 'j' :
+       return "9";
+
+       case 'k':
+       return "10";
+
+       case 'l' :
+       return "11";
+
+       case 'm':
+       return "12";
+
+       case 'n' :
+       return "13";
+
+       //case 'ñ':
+       //return "14";
+
+       case 'o' :
+       return "15";
+
+       case 'p':
+       return "16";
+
+       case 'q' :
+       return "17";
+
+       case 'r':
+       return "18";
+
+       case 's' :
+       return "19";
+
+       case 't':
+       return "20";
+
+       case 'u' :
+       return "21";
+
+       case 'v':
+       return "22";
+
+       case 'w' :
+       return "23";
+
+       case 'x':
+       return "24";
+
+       case 'y' :
+       return "25";
+
+       case 'z':
+       return "26";
+
+       case ' ' :
+       return "27";
+
+       case ',' :
+       return "28";
+
+       case '1' :
+       return "29";
+
+       case '2' :
+       return "30";
+
+       case '3' :
+       return "31";
+
+       case '4' :
+       return "32";
+
+       case '5' :
+       return "32";
+
+       case '6' :
+       return "33";
+
+       case '7' :
+       return "34";
+
+       case '8' :
+       return "35";
+
+       case '9' :
+       return "36";
+
+       default:
+       return "-1";
+     }
+   }//letraNum
+
+   /**
+   *Metodo que me permite transformar un string a una lista de String pero en su forma
+   *numerica para poder despues transformarlos en bigInteger en la codificacion
+   *@param String x
+   *@return String[]
+   */
+
+   public  String[] combertidor(String x){
+     x=x.toLowerCase();
+     String numero="";
+     char apoyo=' ';
+     String [] codificado = new String [x.length()];
+     for (int i=0;i<x.length() ;i++ ) {
+        apoyo=x.charAt(i);
+        codificado[i]=letraNum(apoyo);
+     }
+     return codificado;
+   }
+
+
+  /**
+    Metodo que nos permite cifrar un mensaje
+  */
+  public BigInteger[] mensajeCifrado(String[] mensaje, BigInteger k, BigInteger n){
+    BigInteger [] codificacionFinal= new BigInteger[mensaje.length];
+    BigInteger numero= UNO;
+    BigInteger auxiliar=UNO;
+    for (int i=0;i<codificacionFinal.length ;i++ ) {
+        numero=new BigInteger(mensaje[i]);
+        
+        codificacionFinal[i]=numero.pow(k.intValue()).mod(n);
+    }
+    return codificacionFinal;
+  }
+
+  /**
+  Metodo que permite Decifrar los mensajes de RSA
+  */
+
+  public BigInteger[] desifrarCodificacion(BigInteger[] x, BigInteger j, BigInteger n){
+    BigInteger [] codificacionFinal= new BigInteger[x.length];
+    BigInteger numero= UNO;
+
+    for (int i=0;i<codificacionFinal.length ;i++ ) {
+        //auxiliar=k.intValue();
+        codificacionFinal[i]=(potencia(numero,j,UNO)).mod(n);
+        //codificacionFinal[i]=(numero.pow(j.intValue())).mod(n);
+
+    }
+    return codificacionFinal;
+
+  }
+
+  public static BigInteger potencia(BigInteger base, BigInteger exponente, BigInteger acumulador){
+    if(exponente.compareTo(CERO)==0){
+      return acumulador;
+    }else{
+      return potencia(base,exponente.add(MENOSUNO), acumulador.multiply(base));
+    }
+  }
 
   public static void main(String[] args) {
     RSA prueba= new RSA();
-
     //BigInteger numeros= prueba.creadorLlave();
-    //int x= prueba.cooPrimo(numeros);
-    BigInteger numero1= new BigInteger("115");
-    BigInteger numero2= new BigInteger("224");
-    BigInteger numero3= numero1.divide(numero2);
-    System.out.println(prueba.inversoMultiplicativo(numero1,numero2));
-    //System.out.println(prueba.mcd(numero1,numero2));
+    //
+    BigInteger base = new BigInteger("10000");
+    BigInteger exponente = new BigInteger("2");
+    System.out.println(potencia(base,exponente,UNO));
+    /*String mensaje="hola";
+    BigInteger [] cuartada=prueba.creadorLlave();
+    System.out.println("Buscando un coprimo");
+    int x= prueba.cooPrimo(cuartada[1]);
+    System.out.println("El cooPrimo encontrado es " + x);
+    System.out.println("Por lo que el la clave publica es ");
+    System.out.println("( " + cuartada[1] + ", " + x + " )" );
+    BigInteger k=new BigInteger( x+"");
+    BigInteger j = prueba.inversoMultiplicativo(k,cuartada[1]);
+    System.out.println("Esta es la clave privada " +  j);
+    if((j.compareTo(UNO)==0) || (j.compareTo(MENOSUNO)==0) ){
+      System.out.println("Lo sentimos no hay inverso multiplicativo por favor vuelva a ejecutar");
+      System.out.println("Sentimos las molestias");
 
+    }else{
+      System.out.println("El mensaje sin cifrar es " + mensaje );
+      String [] numeros= prueba.combertidor(mensaje);
+      BigInteger cifradoMensaje[]= prueba.mensajeCifrado(numeros,k,cuartada[0]);
+      System.out.println("Mensaje Cifrado es ");
+      imprimeArrNum(cifradoMensaje);
+      System.out.println("Desifrando el mensaje");
+      BigInteger [] desifrado= prueba.desifrarCodificacion(cifradoMensaje,j,cuartada[0]);
+      imprimeArrNum(desifrado);
+    }*/
+
+
+    //BigInteger numero3= numero1.divide(numero2);
+    //System.out.println(prueba.inversoMultiplicativo(numero1,numero2));
+    //System.out.println(prueba.mcd(numero1,numero2));
+    //String cadena = "paco";
+    //String[] codificacion = prueba.combertidor(cadena);
+    //BigInteger mensajes[]= prueba.mensajeCifrado(cuartada,numero1,numero2);
+
+    //imprimeArr(codificacion);
+    //imprimeArrNum(mensajes);
+    //BigInteger mensajes2[] = prueba.desifrarCodificacion(cuartada,numero1,numero2);
+    //imprimeArrNum(mensajes2);
+    //System.out.println(cadena);
+  }//main
+
+/**
+  Metodo que me permite revisar que la codifcacion numerica este bien
+  *@param String []  x
+*/
+  public static void imprimeArr(String[] x){
+    for (int i=0;i<x.length ; i++ ){
+      System.out.print("| "+ x[i]+" |" );
+    }
+    System.out.println();
+  }
+
+
+  public static void imprimeArrNum(BigInteger[] x){
+    for (int i=0;i<x.length ; i++ ){
+      System.out.print("| "+ x[i]+" |" );
+    }
+    System.out.println();
   }
 
 }
